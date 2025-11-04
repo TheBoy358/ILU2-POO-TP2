43be4eeea58db4418f34d192f2a33b9d88e72367
TheBoy358 <160136222+TheBoy358@users.noreply.github.com>
2025-11-04 11:59:43 +0100

    Add files via upload

---
+++ test/controleur/ControlAfficherVillageTest.java
@@ -0,0 +1,44 @@
+package controleur;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import villagegaulois.Village;
+
+class ControlAfficherVillageTest {
+	private Village village;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irréductibles", 10, 5);
+	}
+
+	@Test
+	void testControlAfficherVillage() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertNotNull(controlAfficherVillage, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testDonnerNomsVillageois() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertEquals(village.donnerVillageois(), controlAfficherVillage.donnerNomsVillageois());
+	}
+
+	@Test
+	void testDonnerNomVillage() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertEquals(village.getNom(), controlAfficherVillage.donnerNomVillage());
+	}
+
+	@Test
+	void testDonnerNbEtals() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertEquals(village.donnerNbEtal(), controlAfficherVillage.donnerNbEtals());
+	}
+
+}
